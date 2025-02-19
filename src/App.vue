f807d9998e2e1af5ff31867b689206277707ac40
CBalthazar <balthclement@gmail.com>
2025-02-19 12:31:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { ref, watch } from 'vue'
+
+const searchText = ref('')
+
+watch(searchText, (val, oldval) => {
+  console.log(`${oldval} => ${val}`)
+})
+</script>
+
+<template>
+  <header>
+    <h1>L'armateur</h1>
+    <div>
+      <!-- missing on change link -->
+      <input type="text" v-model="searchText" />
+    </div>
+    <nav>
+      <RouterLink to="/">Accueil</RouterLink>
+      <RouterLink to="/">Explorer</RouterLink>
+      <RouterLink to="/">Notes</RouterLink>
+      <RouterLink to="/">Communeauté</RouterLink>
+    </nav>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  line-height: 1.5;
+  max-height: 100vh;
+  gap: 2em;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+div {
+  flex-grow: 1;
+  align-self: center;
+  height: 2em;
+  border: 2px solid;
+  border-color: purple;
+  display: flex;
+  flex-direction: row;
+}
+
+div input {
+  flex-grow: 1;
+}
+
+nav {
+  flex: 1 1;
+  display: flex;
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
